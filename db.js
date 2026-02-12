79f3e63fb6b46687b948e304e91b489f8aafa7ad
Yasmin Papini <yasminpapiniii@gmail.com>
2026-02-12 20:56:34 +0000

    fix: atualizando conexão para produção com SSL

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  port: parseInt(process.env.MYSQL_PORT || "3306"),
+port: Number(process.env.MYSQL_PORT) || 17065,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
